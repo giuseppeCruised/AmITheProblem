28fee90f12e17c4bcc0de2cd22f0f46b3752eabf
Adrian Kruse <addrian.niklas@gmail.com>
2022-08-12 23:52:34 +0200

    refactoring git

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blau: {
+          DEFAULT: '#3333FF',
+          50: '#BDC3FF',
+          100: '#ADB4FF',
+          200: '#8F96FF',
+          300: '#7076FF',
+          400: '#5255FF',
+          500: '#3333FF',
+          600: '#140FFF',
+          700: '#0A00EB',
+          800: '#0D00C7',
+          900: '#0E00A3'
+        }
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
